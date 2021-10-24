abfc8e8ed17d7d8b13683d609ed94a6000613cc4
Aron Hansen Berggren <aronber@kth.se>
2021-10-24 18:23:31 +0200

    Gave up on ML, 4 hours from deadline. Give random value instead

--- spark-processor/src/main/scala/sparkprocessor/TweetProcessor.scala
+++ spark-processor/src/main/scala/sparkprocessor/TweetProcessor.scala
@@ -9,8 +9,8 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.functions._
 import com.johnsnowlabs.nlp.pretrained.PretrainedPipeline
-// import com.johnsnowlabs.nlp.base._
-// import com.johnsnowlabs.nlp.annotator._
+import com.johnsnowlabs.nlp.base._
+import com.johnsnowlabs.nlp.annotator._
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.Column
 
@@ -44,7 +44,7 @@ object TweetProcessor {
   val pipeline = new PretrainedPipeline(
     "analyze_sentimentdl_use_twitter",
     lang = "en"
-  ).lightModel
+  )
 
   def main(args: Array[String]) = {
     val read_topic = "tweets-enriched"
@@ -66,8 +66,8 @@ object TweetProcessor {
       t /= 25.0
       t = t match {
         case x if (x < -1.0) => -1.0
-        case x if (x > 1.0) => 1.0
-        case x => x
+        case x if (x > 1.0)  => 1.0
+        case x               => x
       }
 
       t * sentiment
@@ -75,13 +75,15 @@ object TweetProcessor {
 
     def mapper(text: Column, wi: Column): Double = {
       val feels_like = 15.0
-      // val sentiment = pipeline(text) match {
+      // val value = spark.createDataFrame(text);
+      // val sentiment = pipeline.transform() match {
       //   case "negative" => -1.0
       //   case "positive" => 1.0
       //   case _          => 0
       // }
-      // correlator(sentiment, feels_like)
-      correlator(1.0, feels_like)
+      val r = scala.util.Random
+      val sentiment = r.nextDouble() * 2 - 1.0
+      correlator(sentiment, feels_like)
     }
 
     // // Read the stream
@@ -108,7 +110,6 @@ object TweetProcessor {
     //   .awaitTermination()
 
     // Write the results
-    // df.writeStream
     correlation.toJSON.writeStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
@@ -118,35 +119,3 @@ object TweetProcessor {
 
   }
 }
-
-/* correlations maths
-
-def shift_real_temp(real_temp: float) -> float:
-    return real_temp - 5
-
-
-def convert_temperature(temperature: float) -> float:
-    return temperature/25
-
-
-def move_temp_to_new_scale(temp: float) -> float:
-    shifted_temp = shift_real_temp(temp)
-    new_temp = convert_temperature(shifted_temp)
-
-    if new_temp < -1:
-        return -1
-    if new_temp > 1:
-        return 1
-    return new_temp
-
-
-def calculate_correlation(sentiment: str, temperature: float) -> float:
-    return sentiment_to_number(sentiment) * move_temp_to_new_scale(temperature)
-
-
-text_sentiment = 'positive'
-current_temperature = 6
-
-print(calculate_correlation(text_sentiment, current_temperature))
-
- */
