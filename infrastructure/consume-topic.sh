17327ea569a741d5eb366c1d4f9f341f9ac5a374
Felix Seifert <mail@felix-seifert.com>
2021-10-24 20:46:40 +0200

    Delete consume-topic.sh
    Unneeded

--- infrastructure/consume-topic.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker exec -i kafka /bin/bash <<'EOF'
-$KAFKA_HOME/bin/kafka-console-consumer.sh \
-  --bootstrap-server localhost:9092 \
-  --topic correlations \
-  --from-beginning 
-exit
-EOF
